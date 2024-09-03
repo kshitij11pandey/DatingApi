60c9c892dac427769dd6abeedc69f212b6a5f21c
Kshitij Pandey <kshitij1111pandey@gmail.com>
2024-09-03 10:35:25 +0530

    end of stage 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]//apiusers
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users= await context.User.ToListAsync();
+
+        return users;
+
+    }
+
+    [HttpGet("{id:int}")] //api /users/2
+
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+       var user=await context.User.FindAsync(id);
+
+       if(user==null) return NotFound();
+
+       return user;
+    }
+ 
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public  DbSet<AppUser> User{ get; set; }
+}
